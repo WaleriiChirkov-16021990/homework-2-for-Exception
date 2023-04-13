e2a147a028abee9b0e3ca1a5c8531840e57f121a
Валерий Чирков <zx84058405@live.com>
2023-04-13 23:27:00 +0300

    two commit , left last task

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,49 @@
 public class Main {
 	public static void main(String[] args) {
+//		Задание 1
 		System.out.println(user_input());
+		
+//		Задание 2
+//		int d = 0;
+//		int [] array = new int[10];
+//		{код рандомного заполнения массива от 0 до ....}
+//		try {
+//			int catchedRes1 =  d / array[8];
+//			System.out.println("catchedRes1 = 0");
+//		} catch (ArithmeticException e) {
+//			System.out.println("Catching exception: " + e.message);
+//		}
+
+//		или можно рандомизировать d и оставить формулу double catchedRes1 =  array[8] / d;
+//		в ином случае код не имее смысла, в любом случае деля на 0 мы получим пойманное исключение Арифметикэксепшн и никогда не отработавшую 11 строчку.
+
+//		задание 2.1
+		
+//		public static void main(String[] args) throws Exception{
+//			try {
+//				int a = 90;
+//				int b = 3;
+//				System.out.println(a / b);
+//				printSum(23, 234);
+//				int[] abc = { 1, 2 };
+//				abc[3] = 9;
+//			} catch (NullPointerException ex) {
+//				System.out.println("Указатель не может указывать на null!");
+//			} catch (IndexOutOfBoundsException ex) {
+//				System.out.println("Массив выходит за пределы своего размера!");
+//			} catch (Exception ex) {
+//				System.out.println("Что-то пошло не так...");
+//			}
+//		}
+//		public static void printSum(Integer a, Integer b) {
+//			System.out.println(a + b);
+//		}
+
+//		Задание 2.2
+//		Разработайте программу, которая выбросит Exception, когда пользователь вводит пустую строку.
+//		Пользователю должно показаться сообщение, что пустые строки вводить нельзя.
+	
+	
 		
 	}
 	
